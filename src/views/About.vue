51e019da07e5bb6469f5221ade4bb118b5565bf0
takechee <takechee14@gmail.com>
2022-01-16 08:28:52 +0900

    :sparkles: initialize

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="handleClick">Go to Home</button>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { defineComponent } from "vue";
+import router from "../router";
+
+export default defineComponent({
+  name: 'About',
+
+  setup() {
+    const handleClick = () => {
+      router.push('/');
+    };
+
+    return {
+      handleClick,
+    }
+  }
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="handleClick">Go to About</button>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import router from "../router";
+
+export default defineComponent({
+  name: 'Home',
+
+  components: {
+    HelloWorld
+  },
+
+  setup() {
+    const handleClick = () => {
+      router.push('/about');
+    };
+
+    return {
+      handleClick,
+    }
+  }
+});
+</script>
